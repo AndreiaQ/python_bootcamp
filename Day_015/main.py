2775b522e2bcec75649aeacebd055e856dd6f1ad
anbe9 <anbe9@hotmail.com>
2025-01-12 12:15:46 +0000

    Coffee Machine: Make it more dynamic

--- Day_015/main.py
+++ Day_015/main.py
@@ -1,32 +1,40 @@
 from menu import MENU
 
-choice = input("What would you like? (espresso/latte/cappuccino):").lower()
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+    "money": 0,
+}
 
-while choice != "off":
-    drink = MENU
-    resources = {
-        "water": 30,
-        "milk": 200,
-        "coffee": 10,
-        "money": 0
-    }
+
+def check_resources_sufficient(drink):
+    choice_ingredients = MENU[drink]["ingredients"]
+    common_ingredients = choice_ingredients & resources.keys()
+    for ingredients in common_ingredients:
+        if resources[ingredients] < choice_ingredients[ingredients]:
+            print(f"Sorry there is not enough {ingredients}")
+            return False
+    return True
+
+machine_working = True
+
+while machine_working:
+    choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
     if choice == "report":
+        # Print the report
         for key, value in resources.items():
-            if key == 'water' or key == 'milk':
+            if key in ['water', 'milk']:
                 print(f"{key}: {value}ml")
             elif key == "coffee":
                 print(f"{key}: {value}g")
             elif key == "money":
-                print(f"{key}: ${value}")
-        break
-    if choice == "espresso":
-        espresso_ingredients = drink["espresso"]["ingredients"]
-        common_ingredients = resources.keys() & espresso_ingredients.keys()
-        for ingredient in common_ingredients:
-            if resources[ingredient] < espresso_ingredients[ingredient]:
-                print(f"Sorry there is not enough {ingredient}.")
-        break
+                print(f"{key}: ${value:.2f}")
+    elif choice in MENU:
+        check_resources_sufficient(choice)
+        print("Please insert coins:")
 
-    break
-
+    elif choice == "off":
+        machine_working = False
+        print("Bye !")
 
