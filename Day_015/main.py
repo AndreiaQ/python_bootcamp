304271cae5cba99376be879fa644d02bf4dac0d9
anbe9 <anbe9@hotmail.com>
2025-01-12 13:13:09 +0000

    Coffee Machine: Added some functions

--- Day_015/main.py
+++ Day_015/main.py
@@ -7,49 +7,58 @@ resources = {
     "money": 0,
 }
 
+def print_report():
+    for key, amount in resources.items():
+        if key in ['water', 'milk']:
+            print(f"{key}: {amount}ml")
+        elif key == "coffee":
+            print(f"{key}: {amount}g")
+        elif key == "money":
+            print(f"{key}: ${amount:.2f}")
 
 def check_resources_sufficient(drink):
     choice_ingredients = MENU[drink]["ingredients"]
-    common_ingredients = choice_ingredients & resources.keys()
-    for ingredients in common_ingredients:
-        if resources[ingredients] < choice_ingredients[ingredients]:
-            print(f"Sorry there is not enough {ingredients}")
+    matching_ingredients = choice_ingredients & resources.keys()
+    for items in matching_ingredients:
+        if resources[items] < choice_ingredients[items]:
+            print(f"Sorry there is not enough {items}")
             return False
     return True
+
+def calculate_value_inserted():
+    print("Please insert coins:")
+    quarter = int(input("How may quarters?"))
+    dimes = int(input("How may dimes?"))
+    nickles = int(input("How may nickles?"))
+    pennies = int(input("How may pennies?"))
+
+    value_inserted = quarter * 0.25 + dimes * 0.1 + nickles * 0.05 + pennies * 0.01
+
+    return value_inserted
 
 machine_working = True
 
 while machine_working:
     choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
     if choice == "report":
-        # Print the report
-        for key, value in resources.items():
-            if key in ['water', 'milk']:
-                print(f"{key}: {value}ml")
-            elif key == "coffee":
-                print(f"{key}: {value}g")
-            elif key == "money":
-                print(f"{key}: ${value:.2f}")
+       print_report()
     elif choice in MENU:
-        check_resources_sufficient(choice)
-        print("Please insert coins:")
-        quarter = int(input("How may quarters?"))
-        dimes = int(input("How may dimes?"))
-        nickles = int(input("How may nickles?"))
-        pennies = int(input("How may pennies?"))
+        if not check_resources_sufficient(choice):
+            print("Shutting down the coffee machine due to insufficient resources.")
+            machine_working = False
+            continue
 
-        value_inserted = quarter*0.25 + dimes*0.1 + nickles*0.05 + pennies*0.01
-
-        if value_inserted < MENU[choice]["cost"]:
+        value = calculate_value_inserted()
+        if value < MENU[choice]["cost"]:
             print("Sorry that's not enough money. Money refunded.")
         else:
             resources["money"] += MENU[choice]["cost"]
-            change = value_inserted-MENU[choice]["cost"]
+            change = value - MENU[choice]["cost"]
             print(f"Here is ${change:.2f} dollars in change")
             common_ingredients = MENU[choice]["ingredients"] & resources.keys()
             for ingredients in common_ingredients:
                 resources[ingredients] -= MENU[choice]["ingredients"][ingredients]
-            print(f"Here is your {choice}. Enjoy!”")
+            print(f"Here is your {choice}. Enjoy!")
     elif choice == "off":
         machine_working = False
         print("Shutting down coffee machine...")
