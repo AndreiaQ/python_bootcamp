814579e0a124c1e910fa28edf4a1fa1dc04baf14
anbe9 <anbe9@hotmail.com>
2025-02-02 19:04:15 +0000

    Updated challenge 4

--- Day_018/challenge_4.py
+++ Day_018/challenge_4.py
@@ -3,8 +3,8 @@ import random
 
 colormode(255)
 
-turtle = Turtle
-turtle.width(8)
+turtle = Turtle()
+
 
 def random_color():
     r = random.randint(0, 255)
@@ -12,7 +12,25 @@ def random_color():
     b = random.randint(0, 255)
     return r, g, b
 
-turtle.forward(100)
+def random_walk():
+    random_number = random.randint(1,4)
+    if random_number == 1:
+        turtle.forward(100)
+    elif random_number == 2:
+        turtle.back(100)
+    elif random_number == 3:
+        turtle.right(100)
+    elif random_number == 4:
+        turtle.left(100)
+
+
+turtle.width(9)
+turtle.speed(0)
+
+for i in range(100):
+    turtle.color(random_color())
+    random_walk()
+    i += 1
 
 screen = Screen()
 screen.exitonclick()
