4a67d2c675b4b0d756c4d45d978cbb22351ff229
anbe9 <anbe9@hotmail.com>
2025-05-18 17:09:10 +0100

    Saves the ultimate highscore for a txt file

--- Day_020/scoreboard.py
+++ Day_020/scoreboard.py
@@ -1,10 +1,13 @@
 from turtle import Turtle
 
+with open("data.txt") as f:
+    highscore_data = f.read()
 
 class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.highscore = highscore_data
         self.penup()
         self.goto(0,270)
         self.color("white")
@@ -14,13 +17,17 @@ class ScoreBoard(Turtle):
     def update_scoreboard(self):
         """Clears previous score and writes updated score."""
         self.clear()  # Remove old score
-        self.write(f"Scoreboard: {self.score}", align="center", font=("Courier", 18, "bold"))
+        self.write(f"Scoreboard: {self.score} "f"Highscore: {self.highscore}", align="center", font=("Courier", 18, "bold"))
 
     def increase_score(self):
         """Increases the score and updates the scoreboard."""
         self.score += 1
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write(f"Game Over", align="center", font=("Courier", 18, "bold"))
+    def reset(self):
+        if self.score > int(self.highscore):
+            self.highscore = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.highscore))
+        else:
+            self.score = 0
